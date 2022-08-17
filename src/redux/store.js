00cde44143cfc7d94aa0bc6014082df4e000d2de
rabia <rabia.olker@gmail.com>
2022-08-17 20:37:51 +0300

    exported weatherReducer to store.js

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import weatherReducer from "./weatherSlice"
+
+export const store=configureStore({
+    reducer:{
+        weather:weatherReducer,
+    }
+})
