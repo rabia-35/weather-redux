a824e491f4c7ff0561736bc99a077e4f3ee46f48
rabia <rabia.olker@gmail.com>
2022-09-10 21:27:28 +0300

    added default image to background of project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
-import './App.css';
-import Cities from './components/cities';
-import Weather from './components/weather';
-import {useSelector} from "react-redux"
-import Footer from './components/footer';
+import "./App.css";
+import Cities from "./components/cities";
+import Weather from "./components/weather";
+import { useSelector } from "react-redux";
+import Footer from "./components/footer";
 
 function App() {
-  const items=useSelector(state=>state.weather.items)
+  const items = useSelector((state) => state.weather.items);
   let weatherMain;
   let background;
-  let color;
-  if(items){
-    weatherMain=items.current.weather[0].main;
-    if(weatherMain==="Haze" || weatherMain==="Mist"){
-      weatherMain="Fog"
-    }
-    if(weatherMain==="Thunderstorm" || weatherMain==="Thunder"){
-      weatherMain="Rain"
-    }
-    background=`/images/${weatherMain}.jpg`;
 
+  if (items) {
+    weatherMain = items.current.weather[0].main;
+    console.log(weatherMain);
+    if (weatherMain === "Haze" || weatherMain === "Mist") {
+      weatherMain = "Fog";
+    } else if (weatherMain === "Thunderstorm" || weatherMain === "Thunder") {
+      weatherMain = "Rain";
+    } else if (
+      weatherMain !== "Clouds" &&
+      weatherMain !== "Clear" &&
+      weatherMain !== "Fog" &&
+      weatherMain !== "Rain" &&
+      weatherMain !== "Snow"
+    ) {
+      weatherMain = "default";
+    }
+    background = `/images/${weatherMain}.jpg`;
   }
-  
+
   return (
-    <div className='App' style={{  
-      backgroundImage: `url(${background})`,
-    }} >
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
       <Cities />
       <Weather />
       <Footer />
